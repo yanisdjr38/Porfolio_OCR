3666405f74c30dc3314a2a3ab470c067115edd08
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-03-15 00:31:47 +0100

    ajout d'icon avec fontawesome et ajout de style pour les icons

--- portfolio/src/components/Banner.jsx
+++ portfolio/src/components/Banner.jsx
@@ -1,5 +1,8 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
+
 import "../styles/_banner.scss";
 
 function Banner() {
@@ -19,6 +22,36 @@ function Banner() {
       >
         Développeur Web Full Stack
       </motion.p>
+      <ul className="social-icons">
+        <motion.li
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.3 }}
+          className="icon-items"
+        >
+          <a
+            href="https://github.com/yanisdjr38"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </motion.li>
+        <motion.li
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.4 }}
+          className="icon-items"
+        >
+          <a
+            href="https://www.linkedin.com/in/yanis-djouahra/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </motion.li>
+      </ul>
       <div className="banner-description">
         <motion.p
           initial={{ opacity: 0, y: -20 }}
