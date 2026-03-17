33c1adf58b7637be34b785a2de31b9f1728a1336
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-03-17 18:53:09 +0100

    importation des card dans le projet et ajout des liens vers les projets sur github ou les démos en ligne

--- portfolio/src/components/Card.jsx
+++ portfolio/src/components/Card.jsx
@@ -1,21 +1,16 @@
 import "../styles/_card.scss";
 
-function Card({ title, description, image, link }) {
+const Card = ({ title, description, link, image = "path/to/image.jpg" }) => {
   return (
     <div className="card">
-      <img src={image} alt={title} className="card-image" />
-      <h3 className="card-title">{title}</h3>
-      <p className="card-description">{description}</p>
-      <a
-        href={link}
-        className="card-link"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <img src={image} alt={title || "Card Image"} />
+      <p className="heading">{title}</p>
+      <p>{description}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer">
         Voir le projet
       </a>
     </div>
   );
-}
+};
 
 export default Card;
--- portfolio/src/components/Projet.jsx
+++ portfolio/src/components/Projet.jsx
@@ -14,6 +14,7 @@ function Projet() {
             description={project.description}
             image={project.image}
             link={project.link}
+            className="projet-card"
           />
         ))}
       </div>
