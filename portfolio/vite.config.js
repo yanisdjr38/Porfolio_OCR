6cdfb185a58eee579e5273367a66f9d8de9993c0
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-04-16 21:09:16 +0200

    Fix: Netlify build configuration

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  build: {
+    sourcemap: false,
+    minify: "terser",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: false,
+  },
+});
